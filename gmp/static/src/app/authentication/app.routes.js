3b2dafb2021ce2dbe73e722139afdf12c3f04dbf
Oleynik Dmitry <masdar@list.ru>
2016-02-25 17:59:15 +0300

    Good style

--- gmp/static/src/app/authentication/app.routes.js
+++ gmp/static/src/app/authentication/app.routes.js
@@ -36,18 +36,19 @@
 
         $resourceProvider.defaults.stripTrailingSlashes = false;
 
-//        $mdThemingProvider.theme('default').primaryPalette('blue');
+        $mdThemingProvider.theme('default').primaryPalette('blue');
 
-          var blueGreyMap = $mdThemingProvider.extendPalette('blue', {
-          });
-          // Register the new color palette map with the name <code>neonRed</code>
-          $mdThemingProvider.definePalette('blueGrey', blueGreyMap);
-          // Use that theme for the primary intentions
-//          $mdThemingProvider.theme('default')
-//            .primaryPalette('blueGrey');
-        $mdThemingProvider.theme('default').primaryPalette('blueGrey', {
-	  'hue-3': '500', // use shade 600 for the <code>md-hue-2</code> class
-	});
+//          var blueGreyMap = $mdThemingProvider.extendPalette('blue', {
+//            '500': '2e3a4a'
+//          });
+//          // Register the new color palette map with the name <code>neonRed</code>
+//          $mdThemingProvider.definePalette('blueGrey', blueGreyMap);
+//          // Use that theme for the primary intentions
+////          $mdThemingProvider.theme('default')
+////            .primaryPalette('blueGrey');
+//        $mdThemingProvider.theme('default').primaryPalette('blue', {
+//	  'hue-2': '500', // use shade 600 for the <code>md-hue-2</code> class
+//	});
 
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
