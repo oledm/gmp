a929476b839e32ed5365e1235f12807a739d372a
Oleynik Dmitry <masdar@list.ru>
2016-05-04 15:28:58 +0300

    Make initial version of fileupload directive (E)

---
+++ gmp/static/src/app/report/app.report.fileupload.directive.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+
+    angular.module('app.passport')
+        .directive('fileupload', FileUploadDirective);
+
+    function FileUploadDirective(Upload) {
+        'ngInject';
+        function link(scope, el, attrs, ngModel) {
+            el.bind('change', function(e) {
+                angular.forEach(e.target.files, function(file) {
+                    Upload.upload({
+                        url: '/api/upload/',
+                        data: {fileupload: file}
+                    }).
+                    then(function(response) {
+                        if (attrs.multiple !== undefined) {
+                            console.log('Multiple Upload');
+                            ngModel.$viewValue[scope.field].push(response.data.id);
+                            el.append('<span>dsds</span>')
+                        } else {
+                            ngModel.$viewValue[scope.field] = [response.data.id];
+                        }
+                    }, function(response) {
+                        console.log('Error status: ' + response.status);
+                    });
+                });
+            });
+
+//                    scope.$apply(function(){
+//                        ngModel.$setViewValue(el.val());
+//                        ngModel.$render();
+////                        upload(scope.img);
+//                    });
+        }
+        return {
+            require: 'ngModel',
+            restrict: 'E',
+            scope: {
+                ngModel: '=',
+                field: '@',
+                multiple: '@',
+                label: '@'
+            },
+            template: '<input required {{multiple}} type="file" id="{{field}}" name="{{field}}" />',
+            
+            compile: function(tElem, tAttrs, transclude) {
+                var fileInput = angular.element(tElem)[0];
+                if (tAttrs.label) {
+                    var fileLabel = angular.element('<label/>')
+                        .addClass('control-label')
+                        .text(tAttrs.label)
+                        .attr('for', tAttrs.field);
+                    tElem.prepend(fileLabel);
+                }
+                return link;
+            }
+        };
+    }
+})();
