2448de61a1b417d33b15e68b9ebd8d1567f26eee
Oleynik Dmitry <masdar@list.ru>
2016-05-06 17:15:49 +0300

    Partially add validon attr to checkmark

--- gmp/static/src/app/report/app.report.checkmark.directive.js
+++ gmp/static/src/app/report/app.report.checkmark.directive.js
@@ -9,13 +9,25 @@
             require: '^form',
             restrict: 'A',
             link: function(scope, elem, attrs, formController) {
-                if (formController.$name === 'vm.docsForm' ||
-                    formController.$name === 'vm.resultsInfoForm' ||
-                    formController.$name === 'vm.resultsTableForm'
-                    ) {
-                    scope.showCheckmark = true;
-                    return;
-                }
+                console.log('initial valid is ' + attrs.validon);
+//                if (formController.$name === 'vm.docsForm' ||
+//                    formController.$name === 'vm.resultsInfoForm' ||
+//                    formController.$name === 'vm.resultsTableForm'
+//                    ) {
+//                    scope.showCheckmark = true;
+//                    return;
+//                }
+                attrs.$observe('validon', function(newValue) {
+                    console.log('new valid is ' + newValue);
+                    if (!!newValue) {
+                        console.log('new valid is TRUTH!!!!!');
+                        scope.showCheckmark = true;
+                    } else {
+                        console.log('new valid is FALSY!!!!!');
+                        scope.showCheckmark = false;
+                    }
+                    console.log('showCheckmark is ' + scope.showCheckmark);
+                });
                 scope.$watch(formController.$name + '.$valid', function(newValue) {
                     if (newValue) {
                         scope.showCheckmark = true;
